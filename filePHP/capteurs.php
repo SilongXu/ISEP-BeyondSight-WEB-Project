39875669a608bf31e955779d495d5ef6f1583916
Charles <charles_lefever@yahoo.com>
2020-04-18 16:13:12 +0200

    IDK

--- filePHP/capteurs.php
+++ filePHP/capteurs.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html >
-    <?php include "includes/header.php" ?>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="styles.css" />
+        <title>Beyond Sight</title>
+        <link rel="icon" href="images/favicon.ico" />
+    </head>
 
     <script>
-        function remove(idCap){
+        function removeCap(idCap){
               if (str.length == 0) {
     document.getElementById("txtHint").innerHTML = "";
     return;
@@ -29,9 +34,47 @@
 
     <body>
     	<!-- Menu Bar -->
-        <?php include "includes/menubar.php" ?>
+    	<!--<div><img src="images/menu_icon.png" class = "icon_menu"></div>-->
+    	<nav class = "menu_bar">
+    		<div class = "left">
+                <a href="accueil.html">
+                    <div><img src="images/logo_blanc_sans_fleche.png" class = "chrono"></div>
+                    <div><img src="images/logo_blanc_fleche2.png" class = "fleche"></div>
+                </a>
+			</div>
+			<div class = "center">
+                <div><a href="accueil.html" class = "active">Accueil</a></div>
+                <div><a href="forum.html">Forum</a></div>
+                <div><a href="FAQ.html">FAQ</a></div>
+                <div><a href="contact.html">Contact</a></div>
+                
+            </div>
+			<div class = "right">
+				<div><a href="connexion.html">Connexion</a></div>
+				<div><a href="inscription.html">Inscription</a></div>
+                </div>
+			</div>
+            <div class = "langues">
+                <span>Fr</span>
+                <div class = "not_choosen_language">
+                    <ul>
+                        <li><a href="">English</a></li>
+                        <li><a href="">Chinese</a></li>
+                    </ul>
+                </div>
+            </div>
+    	</nav>
+
         <!-- Bouton Search-->
-<?php
+        <div class = "search-box">
+            <input class="search-txt" type="text" name="" placeholder="Rechercher un utilisateur...">
+            <a class="search-btn" href="#">
+                <div><img src="images/loupe.png" class="loupe"></div>
+            </a>
+        </div>
+
+        <div id=tableauCapteur>
+    	<?php
 $servername = "localhost:3308";
 $username = "root";
 $password = "";
@@ -52,7 +95,7 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         $q=$row["idTest"];
-        echo "<tr><td>".$row["idTest"]."</td><td>".$row["capteur"]."</td><td>".$row["test"]."</td><td><button onclick='remove(".$q.")'>Suppr</button></td></tr>";
+        echo "<tr><td>".$row["idTest"]."</td><td>".$row["capteur"]."</td><td>".$row["test"]."</td><td><button onclick='removeCap(".$q.")'>Suppr</button></td></tr>";
     }
     echo "</table>";
 } else {
