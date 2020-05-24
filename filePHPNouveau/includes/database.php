80d6005b14a02b475073ca34f66440a70a0567b3
SilongXu <58178821+SilongXu@users.noreply.github.com>
2020-05-24 21:21:27 +0800

    Update database.php

--- filePHPNouveau/includes/database.php
+++ filePHPNouveau/includes/database.php
@@ -1,16 +1,9 @@
 <?php
-	
-
-	$HOST='localhost:3308';
-
-	$DB_NAME='beyondsight';
-	$USER='root';
-	$PASS='';
 
 	try{
-		$db = new PDO("mysql:host=" . $HOST . ";dbname=" . $DB_NAME,$USER, $PASS);
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //afficher les erreurs
-		//echo 'connecté';
+		$db=new PDO('mysql:host=localhost:3306;dbname=beyondsight','root','');
+		
+		echo 'connecté';
 
 	}catch(PDOException $e){
 		echo $e;
