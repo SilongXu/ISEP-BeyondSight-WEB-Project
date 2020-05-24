b7eaa60d9baa4542fbb377ce72ff567b7bc1753d
SilongXu <58178821+SilongXu@users.noreply.github.com>
2020-05-24 21:20:36 +0800

    Add files via upload

---
+++ filePHPNouveau/logout.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+unset($_SESSION['email']);
+
+header('location: '.'accueil.php');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta cahrset="utf-8">
+</head>
+
+<body>
+    
+</body>
+</html>
